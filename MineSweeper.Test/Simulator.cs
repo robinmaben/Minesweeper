1177a654f78dead5a887809c339e0907a32acb61
Robin Maben <hello@mabenrob.in>
2015-05-10 23:51:25 +0530

    practice exercise; initial commit

---
+++ MineSweeper.Test/Simulator.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MineSweeper.Test
+{
+    [TestClass]
+    public class Simulator
+    {
+        [TestMethod]
+        public void EmptyBoardGameNotOver()
+        {
+            var game = new Game
+            {
+                Grid = new Grid(3, Enumerable.Empty<Point>())
+            };
+
+            Assert.IsTrue(!game.IsLost);
+        }
+
+        [TestMethod]
+        public void BoardWithMinesGameNotOver()
+        {
+            var game = new Game
+            {
+                Grid = new Grid(3, new[]
+                {
+                    new Point(0, 0),
+                    new Point(1, 1)
+                })
+            };
+            
+            Assert.IsTrue(!game.IsLost);
+        }
+
+        [TestMethod]
+        public void GameOverOnFirstMineOpened()
+        {
+            var game = new Game
+            {
+                Grid = new Grid(3, new[]
+                {
+                    new Point(0, 0),
+                    new Point(1, 1)
+                })
+            };
+            
+            var gameResult = game.Move(new Point(0,0), SiteState.Open);
+
+            Assert.IsTrue(gameResult == GameResult.Lost);
+
+        }
+    }
+}
---
+++ MineSweeper/Point.cs
@@ -0,0 +1,16 @@
+namespace MineSweeper
+{
+    public struct Point
+    {
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        //TODO: From String
+
+        public int X;
+        public int Y;
+    }
+}
